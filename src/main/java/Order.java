dd2d3f5bf11b0e7a7f63db1454df36a0d0e06b88
osbort3-TruthO <osbort3@gmail.com>
2025-11-07 13:08:52 -0600

    arraylist created

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,2 +1,15 @@
+import com.pluralsight.Sandwich;
+
+import java.util.ArrayList;
+
 public class Order {
+    private ArrayList<Sandwich> sandwiches;
+    private ArrayList<Chips> chips;
+    private ArrayList<Drink> drinks;
+
+    public Order(ArrayList<Sandwich> sandwiches, ArrayList<Chips> chips, ArrayList<Drink> drinks) {
+        this.sandwiches = sandwiches;
+        this.chips = chips;
+        this.drinks = drinks;
+    }
 }
