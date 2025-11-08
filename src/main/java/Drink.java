7bcffd0ca4e638a04836e8d80daa3d3be38d413e
osbort3-TruthO <osbort3@gmail.com>
2025-11-07 22:58:01 -0600

    drink class done and ui half

--- src/main/java/Drink.java
+++ src/main/java/Drink.java
@@ -1,2 +1,36 @@
 public class Drink {
+
+    private String size;
+    private String flavor;
+    private double price;
+
+    public Drink(String size, String flavor, double price) {
+        this.size = size;
+        this.flavor = flavor;
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,29 @@
 package com.pluralsight;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+        // Color code chatgpt inspired
+        public static final String RESET = "\u001B[0m";
+        public static final String BREAD = "\u001B[33m";   // yellow
+        public static final String LETTUCE = "\u001B[92m"; // green
+        public static final String TOMATO = "\u001B[91m";  // red
+        public static final String CHEESE = "\u001B[93m";  // light yellow
+        public static final String MEAT = "\u001B[31m";// dark red
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        boolean running = true;
+
+        //keep showing menu
+        while (running){
+            showHomeScreen();
+
+            System.out.println("n/Enter Your Choice: ");
+            int choice = input.nextInt();
+            input.nextLine();
+        }
     }
+
 }
